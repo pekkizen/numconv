b656e2d9be9beb65071c7d52eb3fe448c7852fbd
pekkizen <60615893+pekkizen@users.noreply.github.com>
2025-01-12 00:52:27 +0200

    Add files via upload

--- format.go
+++ format.go
@@ -2,24 +2,29 @@ package numconv
 
 import (
 	"encoding/binary"
+	"strconv"
 )
+
+// https://godbolt.org/z/T1drG54az //assembler code from several compilers.
 
 const (
 	maxUint64  = 1<<64 - 1
 	maxUint32  = 1<<32 - 1 // ~4.2e9
 	maxDecimal = 1e17 - 1
-	half       = 0.49999999999999994 // more strconv.FormatFloat compatible than 0.5
+	half       = 0.4999999999999994 // more strconv.FormatFloat compatible than 0.5
+	// half = 0.5
+	// useStrconv = true
 )
 
 /*
-"Magic" constants for transforming integer div n/10, n/100 etc.
+Constants for transforming integer div n/10, n/100 etc.
 to multiply and bitshift operation. Go compiler do something
-similar for constant divs, but these are sligthly faster.
+similar for constant divs and mods, but these are sligthly faster.
 (n * d1) >> e1 == n/10 for  n <= 1<<32 ~ 4.2e9
 (n * d2) >> e1 == n/100 for n <= 1<<32
 (n * d3) >> e3 == n/1e3 for n <= 1<<32
 (n * d4) >> e4 == n/1e4 for n <= 1<<32
-(n * d5) >> e5 == n/1e5 for n < 3e9  	!!
+(n * d5) >> e5 == n/1e5 for n <  3e9  	!!
 (n * d6) >> e6 == n/1e6 for n <= 1<<32
 */
 const (
@@ -29,50 +34,94 @@ const (
 	e4 = 32 + 13
 	e5 = 32 + 16
 	e6 = 32 + 19
+	e7 = 32 + 23
+	e8 = 32 + 26
+
 	d1 = (1<<e1)/10 + 1
 	d2 = (1<<e2)/100 + 1
 	d3 = (1<<e3)/1000 + 1
 	d4 = (1<<e4)/10000 + 1
 	d5 = (1<<e5)/100000 + 1
 	d6 = (1<<e6)/1000000 + 1
+	d7 = (1<<e7)/10000000 + 1
+	d8 = (1<<e8)/100000000 + 1
 )
 
-func divmod1(n uint64) (quo, rem uint64) {
-	if n > maxUint32 {
-		return n / 10, n % 10
-	}
-	return (n * d1) >> e1, n - (n*d1)>>e1*10
-}
-func divmod2(n uint64) (quo, rem uint64) {
-	if n > maxUint32 {
-		return n / 100, n % 100
-	}
-	return (n * d2) >> e2, n - (n*d2)>>e2*100
-}
-func divmod3(n uint64) (quo, rem uint64) {
-	if n > maxUint32 {
-		return n / 1000, n % 1000
-	}
-	return (n * d3) >> e3, n - (n*d3)>>e3*1000
-}
-func divmod4(n uint64) (quo, rem uint64) {
-	if n > maxUint32 {
-		return n / 10000, n % 10000
-	}
-	return (n * d4) >> e4, n - (n*d4)>>e4*10000
-}
-func divmod5(n uint64) (quo, rem uint64) {
-	if n > 3e9 { // 3e9 <  maxUint32 ~ 4.2e9 !!!
-		return n / 100000, n % 100000
-	}
-	return (n * d5) >> e5, n - (n*d5)>>e5*100000
-}
-func divmod6(n uint64) (quo, rem uint64) {
-	if n > maxUint32 {
-		return n / 1000000, n % 1000000
-	}
+// return uint64(f + half) compiles to this with gc 1.22.1:
+// MOVSD $f64.3fdfffffffffffff(SB), X1
+// ADDSD X1, X0
+// MOVSD $f64.43e0000000000000(SB), X1
+// UCOMISD X0, X1
+// JLS main_roundToUint64_pc34
+// CVTTSD2SQ X0, CX
+// NOP
+// JMP main_roundToUint64_pc48
+// main_roundToUint64_pc34:
+// SUBSD X1, X0
+// CVTTSD2SQ X0, CX
+// BTSQ $63, CX
+// main_roundToUint64_pc48:
+// MOVQ CX, AX
+// RET
 
-	return (n * d6) >> e6, n - (n*d6)>>e6*1000000
+func roundToUint64(f float64) uint64 {
+	return uint64(int64(f + half)) // sic, see above
+	// MOVSD $f64.3fdfffffffffffff(SB), X1
+	// ADDSD X1, X0
+	// CVTTSD2SQ X0, AX
+	// RET
+}
+
+// In divmod-functions the compiler creates quite good code for constant integer
+// division and remainder. Compiler code for n/10 - n/1000000 is 20 to 50% slower
+// than my code above. This gain is mostly lost if we have to check for n < maxUint32, even
+// this is 100% of the case. All ASM code is from https://godbolt.org/ with x86-64 gc 1.22.6.
+
+func divmod1(f float64) (q, r uint64) {
+	n := roundToUint64(f * 10)
+	return (n * d1) >> e1, n - (n*d1)>>e1*10
+	// return n / 10, n % 10
+}
+
+func divmod2(f float64) (uint64, uint64) {
+	n := roundToUint64(f * 100)
+	return n / 100, n % 100
+	// MOVQ CX, BX
+	// SHRQ $1, CX
+	// MOVQ $-6640827866535438581, AX
+	// MULQ CX
+	// SHRQ $5, DX
+	// IMUL3Q $100, DX, CX
+	// SUBQ CX, BX
+	// MOVQ DX, AX
+	// RET
+}
+
+func divmod3(f float64) (q, r uint64) {
+	n := roundToUint64(f * 1000)
+	return n / 1000, n % 1000
+}
+
+func divmod4(f float64) (q, r uint64) {
+	n := roundToUint64(f * 10000)
+	return n / 10000, n % 10000
+	// MOVQ $-3335171328526686932, AX
+	// MULQ BX
+	// SHRQ $13, DX
+	// IMUL3Q $10000, DX, CX
+	// SUBQ CX, BX
+	// MOVQ DX, AX
+	// RET
+}
+
+func divmod5(f float64) (q, r uint64) {
+	n := roundToUint64(f * 100000)
+	return n / 100000, n % 100000
+}
+
+func divmod6(f float64) (q, r uint64) {
+	n := roundToUint64(f * 1000000)
+	return n / 1000000, n % 1000000
 }
 
 // Ftoa appends float64 number f as decimal ascii digits to byte slice b.
@@ -82,13 +131,14 @@ func Ftoa(b []byte, f float64, dec int, sep byte) []byte {
 		f = -f
 	}
 	if f >= 1e8 || dec > 8 {
-		return ftoaFull(b, f, dec, sep)
+		b = append(b, strconv.FormatFloat(f, 'f', dec, 64)...)
+		return append(b, sep)
 	}
 	if dec <= 0 {
-		return append(utoa8(b, uint64(f+half)), sep) // no dot
+		return append(utoa8(b, roundToUint64(f)), sep) // no dot
 	}
-	w := upow10[dec]
-	n := uint64(f*fpow10[dec] + half)
+	w := u64pow10[dec]
+	n := roundToUint64(f * f64pow10[dec])
 	q := n / w
 	b = utoa8(b, q)
 	b = append(b, dot)
@@ -99,20 +149,20 @@ func Ftoa(b []byte, f float64, dec int, sep byte) []byte {
 // ftoaFull appends float64 number f as decimal ascii digits to byte slice b.
 // sep is char code (eg. '\t' or ',' or ' ') appended to *b after number.
 // dec is number of decimals.
-func ftoaFull(b []byte, f float64, dec int, sep byte) []byte {
+func FtoaFull(b []byte, f float64, dec int, sep byte) []byte {
 	if dec > 17 {
 		dec = 17
 	}
 	if f > maxDecimal || dec <= 0 {
 		return ftoItoa(b, f, sep)
 	}
-	f *= fpow10[dec]
+	f *= f64pow10[dec]
 	for f > maxDecimal {
 		f /= 10
 		dec--
 	}
-	w := upow10[dec]
-	n := uint64(f + half)
+	w := u64pow10[dec]
+	n := roundToUint64(f)
 	q := n / w
 	b = utoa(b, q)
 	b = append(b, dot)
@@ -128,8 +178,8 @@ func ftoItoa(b []byte, f float64, sep byte) []byte {
 		f /= 10
 		zeros++
 	}
-	b = utoa20(b, uint64(f+half))
-	if zeros > 3 {
+	b = utoa20(b, roundToUint64(f))
+	if zeros > 5 {
 		b = append(b, 'e', '+')
 		b = utoa8(b, uint64(zeros))
 	} else {
@@ -152,27 +202,28 @@ func appendZeros(b []byte, zeros int) []byte {
 }
 
 // Ftoa80 is a faster special Ftoa for f < 1e8 and 0 decimals
-// This is inlineable.
 func Ftoa80(b []byte, f float64, sep byte) []byte {
 	if f < 0 {
 		b = append(b, '-')
 		f = -f
 	}
-	return append(utoa8(b, uint64(f+half)), sep) // no dot
+	b = utoa8(b, uint64(int64(f+half)))
+	return append(b, sep) // no dot
 }
 
+// Ftoa81 is a faster special Ftoa for f < 1e8 and 1 decimal
 func Ftoa81(b []byte, f float64, sep byte) []byte {
 	if f < 0 {
 		b = append(b, '-')
 		f = -f
 	}
-	q, r := divmod1(uint64(f*10 + half))
+	q, r := divmod1(f)
 	b = utoa8(b, q)
 	return append(b, dot, byte('0'+r), sep)
 }
 
 // Ftoa82 is a faster special Ftoa for two decimals and f < 1e8.
-// s = Ftoa82(s, f, 0) is 40 x faster for f=12.34 than
+// s = Ftoa82(s, f, 0) is 45 x faster for f=12.34 than
 // s = append(s, strconv.FormatFloat(f, 'f', 2, 64)...)
 // s = append(s, 0)
 func Ftoa82(b []byte, f float64, sep byte) []byte {
@@ -180,7 +231,7 @@ func Ftoa82(b []byte, f float64, sep byte) []byte {
 		b = append(b, '-')
 		f = -f
 	}
-	q, r := divmod2(uint64(f*100 + half))
+	q, r := divmod2(f)
 	b = utoa8(b, q)
 	return utoaDec2(b, r, sep) // dot, digits, sep
 }
@@ -191,7 +242,7 @@ func Ftoa83(b []byte, f float64, sep byte) []byte {
 		b = append(b, '-')
 		f = -f
 	}
-	q, r := divmod3(uint64(f*1e3 + half))
+	q, r := divmod3(f)
 	b = utoa8(b, q)
 	return utoaDec3(b, r, sep)
 }
@@ -202,7 +253,7 @@ func Ftoa84(b []byte, f float64, sep byte) []byte {
 		b = append(b, '-')
 		f = -f
 	}
-	q, r := divmod4(uint64(f*1e4 + half))
+	q, r := divmod4(f)
 	b = utoa8(b, q)
 	return utoaDec4(b, r, sep)
 }
@@ -213,7 +264,7 @@ func Ftoa85(b []byte, f float64, sep byte) []byte {
 		b = append(b, '-')
 		f = -f
 	}
-	q, r := divmod5(uint64(f*1e5 + half))
+	q, r := divmod5(f)
 	b = utoa8(b, q)
 	return utoaDec5(b, r, sep)
 }
@@ -224,9 +275,9 @@ func Ftoa86(b []byte, f float64, sep byte) []byte {
 		b = append(b, '-')
 		f = -f
 	}
-	q, r := divmod6(uint64(f*1e6 + half))
+	q, r := divmod6(f)
 	b = utoa8(b, q)
-	// utoa6Dec is too big to be inlined. Manually inlined here.
+	// utoa6Dec would be too big to be inlined.
 	q1, q2, q3, q4, q5 := (r*d1)>>e1, (r*d2)>>e2, (r*d3)>>e3, (r*d4)>>e4, (r*d5)>>e5
 	return append(b, dot,
 		byte('0'+q5),
@@ -237,8 +288,8 @@ func Ftoa86(b []byte, f float64, sep byte) []byte {
 		byte('0'+r-q1*10), sep)
 }
 
-// Itoa appends integer i to slice b as ascii digits.
-func Itoa(b []byte, n int, sep byte) []byte {
+// Itoa appends int64 n to slice b as ascii digits.
+func Itoa(b []byte, n int64, sep byte) []byte {
 	if n < 0 {
 		b = append(b, '-')
 		n = -n
@@ -262,6 +313,10 @@ func Utoa8(b []byte, n uint64, sep byte) []byte {
 	return append(utoa8(b, n), sep)
 }
 
+func Utoa(b []byte, n uint64, dec int, sep byte) []byte {
+	return append(utoa(b, n), sep)
+}
+
 // utoa appends uint64 n to slice b as ascii digits.
 func utoa(b []byte, n uint64) []byte {
 	if n < 1e8 {
@@ -279,7 +334,6 @@ func utoaDec(b []byte, n uint64, dec int) []byte {
 }
 
 // utoa20 appends positive integer n as ascii digits to slice b.
-// This is a kind of standard way to do this.
 func utoa20(b []byte, n uint64) []byte {
 	var t [20]byte
 	i := len(t) - 1
@@ -293,7 +347,8 @@ func utoa20(b []byte, n uint64) []byte {
 	return append(b, t[i:]...)
 }
 
-// utoa20Dec appends positive integer n to slice b left zero padded to dec slice digits.
+// utoa20Dec appends positive integer n to slice b left
+// zero padded to dec digits.
 func utoa20Dec(b []byte, n uint64, dec int) []byte {
 	var t [20]byte
 	i := len(t) - 1
@@ -311,8 +366,8 @@ func utoa20Dec(b []byte, n uint64, dec int) []byte {
 const asciiZeros = 0x3030303030303030 // '0'== 0x30
 
 // utoa8 appends positive integer n < 1e8 as ascii digits to slice b.
-// utoa8 is 3+ x faster than utoa20 above. And 10 x faster
-// than b = append(b, strconv.FormatInt(int64(n), 10)...)
+// utoa8 is 3+ x faster for 3 digists than utoa20 above. And 10+ x
+// faster than b = append(b, strconv.Itoa(123)...)
 func utoa8(b []byte, n uint64) []byte {
 	var u uint64
 	digits := 1
@@ -323,6 +378,18 @@ func utoa8(b []byte, n uint64) []byte {
 	u += n + asciiZeros //adds '0'= 0x30 to each byte
 	return binary.LittleEndian.AppendUint64(b, u)[:len(b)+digits]
 }
+
+// utoa3 appends positive integer n < 100 as ascii digits to slice b.
+// func utoa3(b []byte, n uint64) []byte {
+// 	if n < 10 {
+// 		return append(b, byte('0'+n))
+// 	}
+// 	q1, q2 := (n*d1)>>e1, (n*d2)>>e2
+// 	if n < 100 {
+// 		return append(b, byte('0'+q1), byte('0'+n-q1*10))
+// 	}
+// 	return append(b, byte('0'+q2), byte('0'+q1-q2*10), byte('0'+n-q1*10))
+// }
 
 // utoa8Dec appends positive integer n < 1e8 to slice b
 // left zero padded to dec ascii digits.
@@ -355,6 +422,17 @@ func utoaDec3(b []byte, n uint64, sep byte) []byte {
 		byte('0'+q1-q2*10),
 		byte('0'+n-q1*10), sep)
 }
+
+// func utoaDec3(b []byte, n uint64, sep uint64) []byte { // 30% slower than utoaDec3
+// 	q1, q2 := (n*d1)>>e1, (n*d2)>>e2
+// 	u := dot + q2<<8 + (q1-q2*10)<<16 + (n-q1*10)<<24 + sep<<32 + 0x30303000
+// 	return binary.LittleEndian.AppendUint64(b, u)[:len(b)+5]
+// }
+// func utoa3(b []byte, n uint64) []byte { // 30% slower than utoaDec3
+// 	q1, q2 := (n*d1)>>e1, (n*d2)>>e2
+// 	u := q2 + (q1-q2*10)<<6 + (n-q1*10)<<16 + 0x30303000
+// 	return binary.LittleEndian.AppendUint64(b, u)[:len(b)+3]
+// }
 
 // utoaDec4 appends n < 10000 as four left zero padded ascii digits to slice b.
 // Plus decimal dot and number separator sep.
--- parse.go
+++ parse.go
@@ -1,16 +1,20 @@
 package numconv
 
+import "strconv"
+
 const (
 	dot                 = '.'
 	asciiWhitespaceOnly = false
+	maxNumberLen        = 16 // 15 digits + dot or 16 digits
+	// asciiZero           = int64('0')
 )
 
-var fpow10 = [...]float64{
-	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10,
+var f64pow10 = [...]float64{
+	1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10,
 	1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 1e19,
 }
-var upow10 = [...]uint64{
-	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10,
+var u64pow10 = [...]uint64{
+	1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10,
 	1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 1e19,
 }
 
@@ -21,25 +25,32 @@ type err struct {
 func (e *err) Error() string {
 	return "numconv.Atof: " + e.s
 }
-func syntax(b []byte, s string) string {
-	return "parsing " + "\"" + string(b) + "\": " + s
+
+//go:noinline
+func syntaxErr(b []byte) error {
+	return &err{"parsing " + "\"" + string(b) + "\": " + "invalid syntax"}
 }
+
+//go:noinline
+// func syntaxEmptyNumber(b []byte) error {
+// 	return &err{"parsing " + "\"" + string(b) + "\": " + "the number is empty"}
+// }
 
 // Trim returns a subslice of b with all leading and trailing whitespace removed.
 // All characters <= Ascii space (' ' = 32 dec) are left and right trimmed off.
 // This includes, among others, standard Ascii white space ' ', '\t', '\n', '\v', '\f', '\r'.
 func Trim(b []byte) []byte {
 
-	for len(b) > 1 && space(b[0]) { //left trim
+	for len(b) > 1 && isWhitespace(b[0]) { //left trim
 		b = b[1:]
 	}
-	for len(b) > 0 && space(b[len(b)-1]) { //right rim
+	for len(b) > 0 && isWhitespace(b[len(b)-1]) { //right rim
 		b = b[:len(b)-1]
 	}
 	return b
 }
 
-func space(c byte) bool {
+func isWhitespace(c byte) bool {
 	if asciiWhitespaceOnly {
 		return c == ' ' || ('\t' <= c && c <= '\r')
 	}
@@ -54,67 +65,88 @@ func TrimSpace(b []byte) []byte {
 	}
 	l := 0
 	for l = range b {
-		if !space(b[l]) {
+		if !isWhitespace(b[l]) {
 			break
 		}
 	}
 	b = b[l:]
 	r := len(b) - 1
 	for ; r >= 0; r-- {
-		if !space(b[r]) {
+		if !isWhitespace(b[r]) {
 			break
 		}
 	}
 	return b[:r+1]
 }
 
+func TrimTrailingZeros(b []byte) []byte {
+	for len(b) > 1 && b[len(b)-1] == '0' && b[len(b)-2] != dot {
+		b = b[:len(b)-1]
+	}
+	return b
+}
+
+//go:noinline
+func parseFloat(b []byte) (float64, error) {
+	return strconv.ParseFloat(string(b), 64)
+}
+
+//go:noinline
+func parseDigit(c byte) (float64, error) {
+	if '0' <= c && c <= '9' {
+		return float64(int64(c & 15)), nil
+	}
+	return 0, syntaxErr([]byte{c})
+}
+
+func isDigitOrDot(c byte) bool {
+	return ('0' <= c && c <= '9') || c == dot
+}
+
 /*
-Atof parses decimal numbers and integers from byte slice b. In XML files the
+Atof parses a decimal ascii number to float64 from byte slice b. In XML files the
 format is xsd:decimal — Decimal numbers. Valid values include: 123.456,
 +1234.456, -1234.456, -.456, or -456. Integers without decimal dot are also
 accepted. -.0 , -00. 000 and 0 are ok. Use Trim function to remove leading and
 trailing white space. Atof regards it as an error.
-Error is returned for other number formats. Atof(b) is 4+ x faster
-than strconv.ParseFloat(string(b), 64). Up to 15 digits decimals
-Atof gives same as strconv.ParseFloat. For more digits the results
-may be adjacent float64 number.
+Up to 15 digits desimal and 16 digits integer numbers Atof is ~5 x faster
+than strconv.ParseFloat(string(b), 64). Over 15/16 digits Atof calls
+strconv.ParseFloat. Atof is fully compatible with strconv.ParseFloat.
+In a test loop Atof parses 15 digits numbers over 1100 MB/s.
+5 digits numbers at 650 MB/s. Actually Atof only implements a known fast
+parsing path fast and use strconv.ParseFloat for the rest.
 */
 func Atof(b []byte) (float64, error) {
 	if len(b) == 0 {
-		return 0, &err{syntax(b, "number is empty")}
+		return 0, syntaxErr(b)
 	}
-	s := b //keep original for error or AtofFloat
-
-	neg := b[0] == '-'
-	if (neg || b[0] == '+') && len(b) > 1 && b[1] >= dot {
-		b = b[1:] //trim sign if adjacent to dot/number
+	if len(b) == 1 {
+		return parseDigit(b[0])
 	}
-	// Unint64 can hold 19 base 10 digits and we hope that there will be a dot in 20
-	// bytes long number. If it has 20 or more digits and no dot, AtofFloat is used.
-	if len(b) > 20 {
-		b = b[:20]
+	d := b
+	if b[0] == '-' || b[0] == '+' {
+		b = b[1:]
 	}
-	hasDot := false
+	if len(b) > maxNumberLen {
+		return parseFloat(d)
+	}
+	exp10 := 0
 	u := uint64(0)
-	dec := 0
+
 	for i, c := range b {
 		switch {
 		case '0' <= c && c <= '9':
-			u = 10*u + uint64(c-'0')
+			u = u*10 + uint64(c&15)
 
-		case c == dot && !hasDot && len(b) > 1:
-			dec = len(b) - 1 - i //# decimals
-			hasDot = true
+		case c == dot && exp10 == 0 && len(b) > 1:
+			exp10 = len(b) - (i + 1)
 
 		default:
-			return 0, &err{syntax(s, "invalid syntax")}
+			return parseFloat(d)
 		}
 	}
-	if len(b) == 20 && !hasDot {
-		return AtofFloat(s)
-	}
-	f := float64(u) / fpow10[dec]
-	if neg {
+	f := float64(int64(u)) / f64pow10[exp10]
+	if d[0] == '-' {
 		f = -f
 	}
 	return f, nil
@@ -122,53 +154,54 @@ func Atof(b []byte) (float64, error) {
 
 /*
 AtofFloat can parse up to 15 digits decimals and 16 digits decimals integers (IEEE754) correctly.
-Otherwise it mostly behaves like like Atof but gives more adjacent and few next to adjacent values.
 AtofFloat uses simple algorithm with only floating point arithmetic. Amount of parsed digits is
 limited to 308 but accuracy is still limited to the first 16-18 digits.
 Atof is ~1.4 x faster than AtofFloat.
 */
 func AtofFloat(b []byte) (float64, error) {
 	if len(b) == 0 {
-		return 0, &err{syntax(b, "number is empty")}
+		return 0, syntaxErr(b)
 	}
+	d := b
 	neg := b[0] == '-'
-	if (neg || b[0] == '+') && len(b) > 1 && b[1] >= dot { //trim sign
-		b = b[1:]
+	if len(b) > 1 && (neg || b[0] == '+') && isDigitOrDot(b[1]) {
+		b = b[1:] // trim sign
 	}
-	for len(b) > 2 && b[0] == '0' { //trim left zeros
-		b = b[1:]
-	}
+	// for len(b) > 2 && b[0] == '0' { //trim left zeros??
+	// 	b = b[1:]
+	// }
 	var (
 		hasdot = false
-		trunc  = false
-		f      float64
-		pow    = float64(1.0)
-		powdec = float64(1.0)
+		// trunc   = false
+		f       float64
+		pow     = float64(1.0)
+		powfrac = float64(1.0)
 	)
-	if len(b) > 309 {
-		b = b[:309] //1e308 < maxFloat64 < 2e308
-		trunc = true
-	}
+	// if len(b) > 309 {
+	// 	b = b[:309] //1e308 < maxFloat64 < 2e308
+	// 	trunc = true
+	// }
 	for i := len(b) - 1; i >= 0; i-- {
 		c := b[i]
+
 		switch {
 		case '0' <= c && c <= '9':
 			f += pow * float64(c-'0')
 			pow *= 10
 
 		case c == dot && !hasdot && len(b) > 1:
-			powdec = pow
+			powfrac = pow
 			hasdot = true
 
 		default:
-			return 0, &err{syntax(b, "invalid syntax")}
+			return parseFloat(d)
 		}
 	}
-	if trunc && !hasdot {
-		f *= 10 // f -> +Inf
-	}
+	// if trunc && !hasdot {
+	// 	f *= 10 // f -> +Inf
+	// }
 	if neg {
 		f = -f
 	}
-	return f / powdec, nil
+	return f / powfrac, nil
 }
